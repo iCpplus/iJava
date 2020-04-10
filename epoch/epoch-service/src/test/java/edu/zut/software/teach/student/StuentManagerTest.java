aac7423e9e99015628df4b4e88115f47ad8b149b
cpplus <1248281829@qq.com>
2020-04-10 20:02:28 +0800

    update

--- epoch/epoch-service/src/test/java/edu/zut/software/teach/student/StuentManagerTest.java
+++ epoch/epoch-service/src/test/java/edu/zut/software/teach/student/StuentManagerTest.java
@@ -1,5 +1,8 @@
 package edu.zut.software.teach.student;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -9,15 +12,29 @@ import edu.zut.cs.software.teach.impl.StudentManagerImpl;
 import edu.zut.cs.software.teach.service.StudentManager;
 
 public class StuentManagerTest {
+	/**
+	 * Logger for this class
+	 */
+	private static final Logger logger = LogManager.getLogger(StuentManagerTest.class.getName());
 
 	StudentManager studentmanager = new StudentManagerImpl();
 
 	@Test
 	public void testEcho() {
 
+		System.out.println("hellworld");
 		String message = "world";
 		String expected = "hello," + message + "!";
 		String result = this.studentmanager.echo(message);
+
+		if (logger.isInfoEnabled()) {
+			logger.info("testEcho() - String expected={}", expected); //$NON-NLS-1$
+		}
+
+		if (logger.isInfoEnabled()) {
+			logger.info("testEcho() - String result={}", result); //$NON-NLS-1$
+		}
+
 		assertEquals(expected, result);
 
 	}
@@ -29,6 +46,15 @@ public class StuentManagerTest {
 		expected.setName("Lucy");
 		Student result = new Student();
 		result = this.studentmanager.echo(result);
+
+		if (logger.isInfoEnabled()) {
+			logger.info("testEchoStudent() - Student expected={}", expected); //$NON-NLS-1$
+		}
+
+		if (logger.isInfoEnabled()) {
+			logger.info("testEchoStudent() - Student result={}", result); //$NON-NLS-1$
+		}
+
 		assertEquals(expected, result);
 
 	}
