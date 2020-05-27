cfa15f864a76f21183c83e4dc9d786b42effa216
cpplus <1248281829@qq.com>
2020-05-27 19:42:16 +0800

    a

--- epoch/epoch-dao/src/main/java/edu/zut/cs/software/teach/dao/StudentDao.java
+++
@@ -1,8 +0,0 @@
-package edu.zut.cs.software.teach.dao;
-
-import edu.zut.cs.software.base.dao.GenericDao;
-import edu.zut.cs.software.teach.domain.Student;
-
-public interface StudentDao extends GenericDao<Student, Long> {
-
-}
